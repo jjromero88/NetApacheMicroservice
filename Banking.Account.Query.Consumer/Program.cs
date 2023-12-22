9a1d3200e5f0a83dc5742a8b4bdd0d1163108f28
Juan Jose Romero <jromero4@seidor.es>
2023-12-22 14:36:36 -0500

    Creacion de Consumer

---
+++ Banking.Account.Query.Consumer/Program.cs
@@ -0,0 +1,36 @@
+using Banking.Account.Query.Application.Models;
+using Banking.Account.Query.Infrastructure;
+using Banking.Account.Query.Infrastructure.Consumers;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+
+
+builder.Services.Configure<KafkaSettings>(builder.Configuration.GetSection("KafkaSettings"));
+builder.Services.AddInfrastructureService(builder.Configuration);
+builder.Services.AddSingleton<IHostedService, BankAccountConsumerService>();
+
+
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
